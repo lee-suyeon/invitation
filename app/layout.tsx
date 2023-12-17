85cf669a40a01219bc9d8f56fd4be52a981e94d2
suyeon <sroovy@naver.com>
2023-12-17 15:03:22 +0900

    style | global 폰트 교체

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Noto_Sans_KR } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const notoSansKr = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: '수연, 다운 결혼해요 🤍',
-  description: '4월 20일 토요일 오후 1시 30, 인천 샤펠드미앙',
-}
+  title: "수연, 다운 결혼해요 🤍",
+  description: "4월 20일 토요일 오후 1시 30, 인천 샤펠드미앙",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={notoSansKr.className}>{children}</body>
     </html>
-  )
+  );
 }
