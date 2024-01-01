d94879094bc93a4330e4ff1fe6d238994cd9aff1
suyeon <sroovy@naver.com>
2024-01-01 21:55:23 +0900

    feat | account 섹션 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
-import Image from "next/image";
-import Head from "next/head";
-import styles from "./page.module.css";
 import MainSection from "./sections/MainSection";
 import IntroSection from "./sections/IntroSection";
+import GallerySection from "./sections/GallerySection";
+import MessageSection from "./sections/MessageSection";
+import MapSection from "./sections/MapSection";
+import AccountSection from "./sections/AccountSection";
 
 export default function Home() {
   return (
     <div>
       <MainSection />
       <IntroSection />
+      <GallerySection />
+      <MapSection />
+      <MessageSection />
+      <AccountSection />
     </div>
   );
 }
---
+++ app/sections/AccountSection.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { useState, useRef } from "react";
+import classes from "./account-section.module.css";
+
+import classNames from "classnames/bind";
+import Button from "../components/Button/Button";
+const cx = classNames.bind(classes);
+
+function AccountSection() {
+  const [showGroomAccount, setShowGroomAccount] = useState(false);
+  const [showBrideAccount, setShowBrideAccount] = useState(false);
+
+  const brideAccountRef = useRef(null);
+
+  const groomAccountClickHandler = () => {
+    setShowGroomAccount((prev) => !prev);
+  };
+
+  const brideAccountClickHandler = () => {
+    setShowBrideAccount((prev) => !prev);
+  };
+
+  const copyClickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+  };
+
+  return (
+    <div className={classes.account}>
+      <div className={classes.title}>
+        <h3>마음 전하실 곳</h3>
+        <p>
+          참석이 어려우신 분들을 위해 <br />
+          계좌번호를 기재하였습니다. <br />
+          너그러운 마음으로 양해부탁드립니다.
+        </p>
+      </div>
+      <div>
+        <div className={cx("account-btn")} onClick={groomAccountClickHandler}>
+          신랑측 계좌번호 보기
+          {showGroomAccount && (
+            <div>
+              <div className={classes.bar} />
+              <div className={classes.panel}>
+                <span>국민은행 123-456-789 김다운</span>
+                <Button color="primary" onClick={copyClickHandler}>
+                  복사
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className={cx("account-btn")} onClick={brideAccountClickHandler}>
+          신부측 계좌번호 보기
+          {showBrideAccount && (
+            <div>
+              <div className={classes.bar} />
+              <div className={classes.panel}>
+                <div>
+                  <span>우리은행</span>
+                  <span ref={brideAccountRef}>123-456-789</span>
+                  <span>이수연</span>
+                </div>
+                <Button color="primary" onClick={copyClickHandler}>
+                  복사
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AccountSection;
