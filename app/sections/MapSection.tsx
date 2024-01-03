b8f7cf88fdc25b9b76dbf5222eddc30cbc5549cd
suyeon <sroovy@naver.com>
2024-01-03 22:48:34 +0900

    feat | 오시는 길 정보 추가

--- app/sections/MapSection.tsx
+++ app/sections/MapSection.tsx
@@ -44,13 +44,30 @@ function MapSection() {
   }, []);
 
   return (
-    <div className={classes.map}>
+    <section className={classes.map}>
       <Title>
         <h3>오시는 길</h3>
       </Title>
       <div className={classes.maparea} id="map"></div>
-      <div className={classes.info}></div>
-    </div>
+      <ul className={classes.info}>
+        <li>
+          <span className={classes.title}>주소</span>
+          <span>인천광역시 남동구 정각로 10 샤펠드미앙</span>
+        </li>
+        <li>
+          <span className={classes.title}>지하철</span>
+          <span>인천2호선 인천시청역 3번출구에서 300m</span>
+        </li>
+        <li>
+          <span className={classes.title}>버스</span>{" "}
+          <span>13번 123번 504번 </span>
+        </li>
+        <li>
+          <span className={classes.title}>주차</span>{" "}
+          <span>인천시청 주차장 이용</span>
+        </li>
+      </ul>
+    </section>
   );
 }
 
